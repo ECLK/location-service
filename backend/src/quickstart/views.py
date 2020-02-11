6359ed6a58e44ca4b39ce550efc5f329543a79e2
MrClemRkz <clement.amila@gmail.com>
2020-02-11 13:41:41 +0530

    add example endpoints

--- backend/src/quickstart/views.py
+++ backend/src/quickstart/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from src.quickstart.serializers import UserSerializer, GroupSerializer
 
-# Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API Endpoint for User view and edit
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API Endpoint for Group view and edit
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
