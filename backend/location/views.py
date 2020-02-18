bb451621ac154a7606fc89aad7b8b2a0abfbc6c5
JASSJayasinghe-ECSL <jassjgit@gmail.com>
2020-02-18 16:52:06 +0530

    View were added

--- backend/location/views.py
+++ backend/location/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from rest_framework.pagination import PageNumberPagination
+from rest_framework import viewsets
+from rest_framework import mixins
+from rest_framework.permissions import IsAuthenticated
+from .models import Admindistrict, Electroaldistrict, Polingdivision, Gramaniladaridivision, Locations
+from .serializers import AdmindistrictSerializer, ElectroaldistrictSerializer, PolingdivisionSerializer, GramaniladaridivisionSerializer, LocationsSerializer
 
 # Create your views here.
+
+class AdmindistrictViewset(mixins.CreateModelMixin,mixins.ListModelMixin,mixins.UpdateModelMixin,mixins.RetrieveModelMixin,viewsets.GenericViewSet):
+    queryset = Admindistrict.objects.all()
+    serializer_class = AdmindistrictSerializer
+    pagination_class = PageNumberPagination
+    permission_classes = (IsAuthenticated,)
+
+class ElectoraldistrictViewset(mixins.CreateModelMixin,mixins.ListModelMixin,mixins.UpdateModelMixin,mixins.RetrieveModelMixin,viewsets.GenericViewSet):
+    queryset = Electroaldistrict.objects.all()
+    serializer_class = ElectroaldistrictSerializer
+    pagination_class = PageNumberPagination
+    permission_classes = (IsAuthenticated,)
+
+class PolingdivisionViewset(mixins.CreateModelMixin,mixins.ListModelMixin,mixins.UpdateModelMixin,mixins.RetrieveModelMixin,viewsets.GenericViewSet):
+    permission_classes = [IsAuthenticated,]
+    queryset = Polingdivision.objects.all()
+    serializer_class = PolingdivisionSerializer
+    pagination_class = PageNumberPagination
+    
+
+class LocationViewset(mixins.CreateModelMixin,mixins.ListModelMixin,mixins.UpdateModelMixin,mixins.RetrieveModelMixin,viewsets.GenericViewSet):   
+    queryset = Locations.objects.all()
+    serializer_class = LocationsSerializer
+    permission_classes = (IsAuthenticated,)
+    pagination_class = PageNumberPagination
+
+class GramaniladariViewset(mixins.CreateModelMixin,mixins.ListModelMixin,mixins.UpdateModelMixin,mixins.RetrieveModelMixin,viewsets.GenericViewSet):
+    permission_classes = (IsAuthenticated,)
+    queryset = Gramaniladaridivision.objects.all()
+    serializer_class = GramaniladaridivisionSerializer
+    pagination_class = PageNumberPagination
+    
+
