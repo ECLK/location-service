239f8ba135d0b3226b2e87f9fd35c1028368e73e
JASSJayasinghe-ECSL <jassjgit@gmail.com>
2020-02-19 09:03:59 +0530

    Image upload added

--- backend/location/views.py
+++ backend/location/views.py
@@ -2,9 +2,9 @@ from rest_framework.pagination import PageNumberPagination
 from rest_framework import viewsets
 from rest_framework import mixins
 from rest_framework.permissions import IsAuthenticated
-from .models import Admindistrict, Electroaldistrict, Polingdivision, Gramaniladaridivision, Locations
-from .serializers import AdmindistrictSerializer, ElectroaldistrictSerializer, PolingdivisionSerializer, GramaniladaridivisionSerializer, LocationsSerializer
-
+from .models import Admindistrict, Electroaldistrict, Polingdivision, Gramaniladaridivision, Locations,Media_items
+from .serializers import AdmindistrictSerializer, ElectroaldistrictSerializer, PolingdivisionSerializer, GramaniladaridivisionSerializer, LocationsSerializer, Media_itemsSerializer
+from rest_framework.parsers import FileUploadParser
 # Create your views here.
 
 class AdmindistrictViewset(mixins.CreateModelMixin,mixins.ListModelMixin,mixins.UpdateModelMixin,mixins.RetrieveModelMixin,viewsets.GenericViewSet):
@@ -12,7 +12,6 @@ class AdmindistrictViewset(mixins.CreateModelMixin,mixins.ListModelMixin,mixins.
     serializer_class = AdmindistrictSerializer
     pagination_class = PageNumberPagination
     permission_classes = (IsAuthenticated,)
-
 class ElectoraldistrictViewset(mixins.CreateModelMixin,mixins.ListModelMixin,mixins.UpdateModelMixin,mixins.RetrieveModelMixin,viewsets.GenericViewSet):
     queryset = Electroaldistrict.objects.all()
     serializer_class = ElectroaldistrictSerializer
@@ -25,17 +24,21 @@ class PolingdivisionViewset(mixins.CreateModelMixin,mixins.ListModelMixin,mixins
     serializer_class = PolingdivisionSerializer
     pagination_class = PageNumberPagination
     
-
 class LocationViewset(mixins.CreateModelMixin,mixins.ListModelMixin,mixins.UpdateModelMixin,mixins.RetrieveModelMixin,viewsets.GenericViewSet):   
     queryset = Locations.objects.all()
     serializer_class = LocationsSerializer
     permission_classes = (IsAuthenticated,)
     pagination_class = PageNumberPagination
-
 class GramaniladariViewset(mixins.CreateModelMixin,mixins.ListModelMixin,mixins.UpdateModelMixin,mixins.RetrieveModelMixin,viewsets.GenericViewSet):
     permission_classes = (IsAuthenticated,)
     queryset = Gramaniladaridivision.objects.all()
     serializer_class = GramaniladaridivisionSerializer
     pagination_class = PageNumberPagination
-    
+class MideaItemViewset(mixins.CreateModelMixin,mixins.ListModelMixin,mixins.RetrieveModelMixin,viewsets.GenericViewSet):
+    permission_classes = (IsAuthenticated,)
+    queryset = Media_items.objects.all()
+    parser_class = (FileUploadParser,)
+    serializer_class = Media_itemsSerializer
+    pagination_class = PageNumberPagination
+
 
