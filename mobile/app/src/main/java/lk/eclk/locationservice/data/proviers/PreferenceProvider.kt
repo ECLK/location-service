82ace44bf56d5e160bbb9c0a4083fe54be8e99b0
hirantha <sahanhirantha@gmail.com>
2020-02-21 20:32:37 +0530

    jwt token shared preference provider

---
+++ mobile/app/src/main/java/lk/eclk/locationservice/data/proviers/PreferenceProvider.kt
@@ -0,0 +1,14 @@
+package lk.eclk.locationservice.data.proviers
+
+import android.content.Context
+import android.content.SharedPreferences
+import androidx.preference.PreferenceManager
+
+abstract class PreferenceProvider(context: Context) {
+    // shared preference for cache data
+    private val appContext = context.applicationContext
+    protected val preference: SharedPreferences
+        get() = PreferenceManager.getDefaultSharedPreferences(
+            appContext
+        )
+}
