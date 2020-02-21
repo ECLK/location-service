c97327e9a83429568ddfa821ff4d864b40cbae23
hirantha <sahanhirantha@gmail.com>
2020-02-21 22:49:11 +0530

    sign in fragment added

--- mobile/app/src/main/java/lk/eclk/locationservice/ui/signin/SignInViewModelFactory.kt
+++ mobile/app/src/main/java/lk/eclk/locationservice/ui/signin/SignInViewModelFactory.kt
@@ -1,14 +1,14 @@
-package lk.eclk.locationservice.ui.splash
+package lk.eclk.locationservice.ui.signin
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import lk.eclk.locationservice.data.Repository
 
-class SplashScreenViewModelFactory(private val repository: Repository) :
+class SignInViewModelFactory(private val repository: Repository) :
     ViewModelProvider.NewInstanceFactory() {
 
     @Suppress("UNCHECKED_CAST")
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-        return SplashScreenViewModel(repository) as T
+        return SignInViewModelFactory(repository) as T
     }
 }
