a0d397c59dcd95662ebcf443a2c2f5006efb3777
JASSJayasinghe-ECSL <jassjgit@gmail.com>
2020-02-24 12:49:32 +0530

    Search function added

--- backend/location/views.py
+++ backend/location/views.py
@@ -5,6 +5,8 @@ from rest_framework.permissions import IsAuthenticated
 from .models import Admindistrict, Electroaldistrict, Polingdivision, Gramaniladaridivision, Locations,Media_items
 from .serializers import AdmindistrictSerializer, ElectroaldistrictSerializer, PolingdivisionSerializer, GramaniladaridivisionSerializer, LocationsSerializer, Media_itemsSerializer
 from rest_framework.parsers import FileUploadParser
+from rest_framework.filters import SearchFilter, OrderingFilter
+from rest_framework.response import Response
 # Create your views here.
 
 class AdmindistrictViewset(mixins.CreateModelMixin,mixins.ListModelMixin,mixins.UpdateModelMixin,mixins.RetrieveModelMixin,viewsets.GenericViewSet):
@@ -40,5 +42,19 @@ class MideaItemViewset(mixins.CreateModelMixin,mixins.ListModelMixin,mixins.Retr
     parser_class = (FileUploadParser,)
     serializer_class = Media_itemsSerializer
     pagination_class = PageNumberPagination
+class SearchLocation(mixins.ListModelMixin,mixins.RetrieveModelMixin,viewsets.GenericViewSet):
+    permission_classes = (IsAuthenticated,)
+    queryset = Locations.objects.all()
+    serializer_class = LocationsSerializer
+    filter_backends = [SearchFilter]
+    search_fields = ['code', 'name_sinhala','name_tamil','name_english','gdn__gnd_code', 'gdn__name_english','gdn__name_sinhala','gdn__name_tamil','gdn__polingdivision__name_english','gdn__polingdivision__name_tamil','gdn__polingdivision__name_sinhala','gdn__polingdivision__id','gdn__polingdivision__electoral_district__name_sinhala','gdn__polingdivision__electoral_district__name_english','gdn__polingdivision__electoral_district__name_tamil','gdn__polingdivision__electoral_district__id']
+
+
+    
+    
+    
+
+    
+    
 
 
