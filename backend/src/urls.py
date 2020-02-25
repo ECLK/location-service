c07d1d604484cb2ba869e9bf3ee4ba70eab96ced
JASSJayasinghe-ECSL <jassjgit@gmail.com>
2020-02-25 17:57:37 +0530

    swagger doc added

--- backend/src/urls.py
+++ backend/src/urls.py
@@ -5,13 +5,18 @@ from rest_framework_simplejwt import views as jwt_views
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls import url
+from rest_framework_swagger.views import get_swagger_view
+
+schema_view = get_swagger_view(title='Location Service API')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('location.urls')),
     path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
-
+    path('doc/', schema_view)
 ]
 
 if settings.DEBUG:
